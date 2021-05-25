4972ab62ea6506788fb3dcc0516f8a11b7d0936a
Victor Manuel Delfin Santos <victormanu31611@gmail.com>
2021-05-24 20:35:17 -0500

    Advanced response for score control

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 const app = require('express')();
 const cors = require("cors");
-const {didIWin,getIntegerForm} = require("./game")
-const {getMoves,cleanRoom,creteRoom,getPlayersCount,findRoom} =  require("./roomFuntions")
+// const {didIWin,getIntegerForm} = require("./game")
+const {
+    printAllRooms,
+    creteRoom,
+    joinCreatedRoom,
+    isRoomTaken,
+    makeAMove,
+    cleanRoom
+} =  require("./roomFuntions")
 
 app.use(cors());
 const server = require('http').createServer(app);
@@ -12,47 +19,39 @@ const io = require('socket.io')(server,{
       }    
 });
 
-
 server.listen(3000);
 io.on('connection', (socket) => { 
     console.log("new Client: ",socket.id)
     
     socket.on("joinRoom",(data) => {
         console.log(`${data.name} wants to join room: ${data.room}`)
-
-        // TEST GET PLAYERS
-        // getPlayersCount("1")
         
-        // TEST FIND ROOM
-        // findRoom("2")
-        
-        //join group
-        const checkRoomCreated = findRoom(data.room)
-        
-        if(checkRoomCreated){
+        //check if room exist
+        if(!isRoomTaken(data.room)){
+            //join group
             socket.join(data.room);
 
             //Send to other player in the group
             io.to(data.room).emit("joinRoom",{name:data.name,room:data.room});
 
             //add the room with a basic data
-            creteRoom(data.room)
+            creteRoom(data.room,data.name)
         }else{
-            
+            socket.join(data.room);
+            io.to(data.room).emit("joinRoom",{name:data.name,room:data.room});
+            joinCreatedRoom(data.room,data.name)
         }
-
- 
-        
     })
 
     socket.on("newMove",(data)=>{
         console.log(`${data.name} picked: ${data.option} for room: ${data.room}`)
     
-        //get the data from the room
-        const roomDetails = getMoves(data.room,data.name,data.option)
+        //Make the move
+        const afterMove = makeAMove(data.room,data.name,data.option)
 
         //send the data to the client
-        io.to(data.room).emit("newMove",{roomDetails});
+        io.to(data.room).emit("newMove",{afterMove});
+        console.log(isRoomTaken("1"), "\n\n")
     })
 
     socket.on("cleanRoom",(data)=>{
@@ -60,10 +59,8 @@ io.on('connection', (socket) => {
         
         //clean the player1, player2 and moves from the room
         const newRoomClean = cleanRoom(data.room)
+        console.log(newRoomClean)
         
-
-        // console.log(newRoomClean)
-
         //send the clean room to the client so it can restart the board
         io.to(data.room).emit("cleanRoom",{newRoomClean});
     })
--- roomFuntions.js
+++ roomFuntions.js
@@ -1,28 +1,28 @@
 const {didIWin,getIntegerForm} = require("./game")
 const rooms = [
-    {
-        room:"1",
-        name:"11",
-        players:[
-            {name:"Player11"},
-            {name:"11Player"}
-        ]
-    },
-    {
-        room:"2",
-        name:"22",
-        players:[
-            {name:"Player22"},
-        ]
-    },
-    {
-        room:"3",
-        name:"33",
-        players:[
-            {name:"Player33"},
-            {name:"33Player"}
-        ]
-    }
+    // {
+    //     room:"1",
+    //     name:"11",
+    //     players:[
+    //         {name:"Player11"},
+    //         {name:"11Player"}
+    //     ]
+    // },
+    // {
+    //     room:"2",
+    //     name:"22",
+    //     players:[
+    //         {name:"Player22"},
+    //     ]
+    // },
+    // {
+    //     room:"3",
+    //     name:"33",
+    //     players:[
+    //         {name:"Player33"},
+    //         {name:"33Player"}
+    //     ]
+    // }
 ]
 
 
@@ -40,7 +40,8 @@ exports.creteRoom = (room,playerName) => {
         moves:0,
         player1won:false,
         players:[
-            {player:1,name:playerName,option:"",score:0}
+            {player:1,name:playerName,option:"",score:0},
+            {player:2,name:"",option:"",score:0}
         ]
     }
     
@@ -52,7 +53,8 @@ exports.joinCreatedRoom = (room,playerName) =>{
     rooms.forEach(elementRoom => {
         if(elementRoom.room === room){
             const secondPlayer = {player:2,name:playerName,option:"",score:0}
-            elementRoom.players.push(secondPlayer)
+            elementRoom.players[1] = secondPlayer
+            console.log("\n[[Room function]] Has enter",elementRoom,"\n")
         }
     })
 }
@@ -73,9 +75,11 @@ exports.makeAMove = (room,name,option) =>{
             if(element.players[0].name===name){
                 const playerInfo = element.players[0]
                 element.players[0] = {...playerInfo, option}
+                element.waiting=1
             }else{                
                 const playerInfo = element.players[1]
                 element.players[1] = {...playerInfo, option}
+                element.waiting=2
             }
             if(element.moves===3){
                 element.moves=1
@@ -86,12 +90,15 @@ exports.makeAMove = (room,name,option) =>{
                 const player1ElectionInt = getIntegerForm(element.players[0].option)
                 const player2ElectionInt = getIntegerForm(element.players[1].option)
                 const player1won = didIWin(player1ElectionInt,player2ElectionInt)
+                winner="IT IS A TIE"
                 if(player1won===1){
                     element.players[0].score = element.players[0].score+1 
+                    element.winner = element.players[0].name + " WINS"
                 }else if(player1won===-1){
                     element.players[1].score = element.players[1].score+1     
+                    element.winner = element.players[1].name+ " WINS"
                 }
-                
+                element.waiting = 0
             }
             
             return element
@@ -99,10 +106,13 @@ exports.makeAMove = (room,name,option) =>{
     });
 }
 exports.cleanRoom = (room) =>{
-    rooms.forEach(element => {
-        if(element.room==room){
+    return rooms.find(element => {
+        if(element.room===room){
+            console.log(element)
+            element.moves = 0
             element.players[0].option =""
             element.players[1].option =""
+            return element
         }
     });
 }
