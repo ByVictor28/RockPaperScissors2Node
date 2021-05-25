a14b358e57cbc846b6f03342a540790dd1fba809
Victor Manuel Delfin Santos <victormanu31611@gmail.com>
2021-05-24 23:51:40 -0500

    hide logs

--- index.js
+++ index.js
@@ -24,10 +24,10 @@ const io = require('socket.io')(server,{
 
 server.listen(3000);
 io.on('connection', (socket) => { 
-    console.log("new Client: ",socket.id)
+    // console.log("new Client: ",socket.id)
     
     socket.on("joinRoom",(data) => {
-        console.log(`${data.name} wants to join room: ${data.room}`)
+        // console.log(`${data.name} wants to join room: ${data.room}`)
         
         //check if room exist
         if(!isRoomTaken(data.room)){
@@ -60,35 +60,35 @@ io.on('connection', (socket) => {
     })
 
     socket.on("leaveGroup",({room,name}) => {
-        console.log("LEAVE" , room,name)
+        // console.log("LEAVE" , room,name)
         const newRoom = leaveRoom(room,name)
 
         io.to(room).emit("leaveGroup",newRoom)
     })
 
     socket.on("newMove",(data)=>{
-        console.log(`${data.name} picked: ${data.option} for room: ${data.room}`)
+        // console.log(`${data.name} picked: ${data.option} for room: ${data.room}`)
     
         //Make the move
         const afterMove = makeAMove(data.room,data.name,data.option)
 
         //send the data to the client
         io.to(data.room).emit("newMove",{afterMove});
-        console.log(isRoomTaken("1"), "\n\n")
+        // console.log(isRoomTaken("1"), "\n\n")
     })
 
     socket.on("cleanRoom",(data)=>{
-        console.log("CLEAN ROOM",data.room)
+        // console.log("CLEAN ROOM",data.room)
         
         //clean the player1, player2 and moves from the room
         const newRoomClean = cleanRoom(data.room)
-        console.log(newRoomClean)
+        // console.log(newRoomClean)
         
         //send the clean room to the client so it can restart the board
         io.to(data.room).emit("cleanRoom",{newRoomClean});
     })
     
     socket.on('disconnect', () => {
-            console.log("Adios")
+            // console.log("Adios")
     });      
  });
